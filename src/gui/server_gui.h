be4de82290b6d3c7d297271d31cf18a865c32a96
gigi333-d <giestruquel@gmail.com>
2026-05-23 21:30:32 -0700

    Add header for server GUI functions

--- src/gui/server_gui.h
+++ src/gui/server_gui.h
@@ -1,1 +1,12 @@
-
+#ifndef SERVER_GUI_H
+#define SERVER_GUI_H
+ 
+#include "game_state.h"
+ 
+// call once after init_game_state() to build and show the server window
+void launch_server_window(GameState *game);
+ 
+// call after any game state change to update all widgets on screen
+void server_gui_refresh(void);
+ 
+#endif
